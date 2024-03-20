ff3f6ec2af221ec83e98c9863c6dffee4beced67
zhirui-gao <gzr990521@163.com>
2024-03-20 17:09:37 +0800

    Update readme

--- autoSyn.sh
+++ autoSyn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git status  
 git add *  
-git commit -m 'Update'
+git commit -m 'Update readme'
 git pull --rebase origin master   #domnload data
 git push origin master            #upload data
 git stash pop
